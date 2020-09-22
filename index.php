60c5c0a754e2c5d6dc1682023e4fae221f3875bd
taku2846 <hpvmn7059duj@gmail.com>
2020-09-22 16:48:45 +0900

    index.phpの作成

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calender</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th><a href="">&laquo;</a></th>
+                <th colspan="5">August</th>
+                <th><a href="">&raquo;</a></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Sun</td>
+                <td>Mon</td>
+                <td>Tue</td>
+                <td>Wed</td>
+                <td>Thu</td>
+                <td>Fri</td>
+                <td>Sat</td>
+            </tr>
+            <tr>
+                <td class="youbi_0">1</td>
+                <td class="youbi_1">2</td>
+                <td class="youbi_2">3</td>
+                <td class="youbi_3">4</td>
+                <td class="youbi_4 today">5</td>
+                <td class="youbi_5">6</td>
+                <td class="youbi_6">8</td>
+            </tr>
+            <tr>
+                <td class="youbi_0">30</td>
+                <td class="youbi_1">31</td>
+                <td class="gray">1</td>
+                <td class="gray">2</td>
+                <td class="gray">3</td>
+                <td class="gray">4</td>
+                <td class="gray">6</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="7"><a href="">Today</a></th>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
+</html>
